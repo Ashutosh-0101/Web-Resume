5d6d6fb28b14e6e4cf50d273bb4b7e2c2e9b42c2
Ashutosh Kumar <104302587+Ashutosh-0101@users.noreply.github.com>
2023-07-24 18:14:16 +0530

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
         $(target).addClass("show");
     });
 
-    // $("#swift a").click(function(){
-    //     // $("section").removeClass("show");
-    //     var target = $(this).attr("href");
-    //     $(target).addClass("show");
-    // });
+    $("#swift a").click(function(){
+        // $("section").removeClass("show");
+        var target = $(this).attr("href");
+        $(target).addClass("show");
+    });
 
-    // $(".type-effect").typed({
-    //     strings: ["Student" ,"Developer"],
-    //     typeSpeed: 250,
-    //     backSpeed:60, 
-    //     loop: true
-    // })
+    $(".type-effect").typed({
+        strings: ["Student" ,"Developer"],
+        typeSpeed: 250,
+        backSpeed:60, 
+        loop: true
+    })
 });
